6ef70e77b22980c90b655b2b6a89f20c9179bd4d
yathu1996 <sulojana1107@gmail.com>
2020-09-18 05:47:30 +0530

    HI

--- CscExample.java
+++ CscExample.java
@@ -5,3 +5,27 @@ public class CscExample{
 	String text;
 
 }
+public void setQuestionNo(int questionNO){
+	this.questionNO =questionNO;
+	
+}
+public void setQCategory(String category){
+	this.category==category;
+	
+}
+public void setText(String text){
+	this.text==text;
+	
+}
+public int getQuestionNo(){
+	return questionNO;
+	
+}
+public String getCategory(){
+	return category;
+	
+}
+public String getText(){
+	return text;
+	
+}
